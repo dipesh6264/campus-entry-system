7e48a7e245589292c047c45f9a56314d08f4a545
Dipesh6264 <dipesh.patidar@iitgn.ac.in>
2024-06-30 22:38:38 +0530

    files added

---
+++ temp/qy3mesv.cpp
@@ -0,0 +1,14 @@
+// Create a function
+#include <iostream>
+using namespace std;
+
+void myFunction() {
+  cout << "I just got executed!";
+}
+
+int main() {
+  myFunction(); // call the function
+  return 0;
+}
+
+// Outputs "I just got executed!"
---
+++ temp/ta2metf.cpp
@@ -0,0 +1,12 @@
+// Create a function
+#include <iostream>
+void myFunction() {
+  cout << "I just got executed!";
+}
+
+int main() {
+  myFunction(); // call the function
+  return 0;
+}
+
+// Outputs "I just got executed!"
